fc7dd0ac4f469bbef0fc097b169d36a4b73d889c
Nguyen Viet Nguyen <nnguyen@2clicksolutions.com>
2021-08-30 18:51:31 +0700

    Add localization supported

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/configurations/dependency_configuration.dart';
 import 'package:todo/gui/todo_home_page.dart';
@@ -14,9 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo application',
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
       debugShowCheckedModeBanner: false,
       theme: CustomTheme.lightTheme,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: ChangeNotifierProvider(
         create: (_) => TodoHomePageViewModel(),
         child: TodoHomePage(),
